ae15a94a3045513b5c259108e46ec33b9d9e6824
garayf <edgarayf@unal.edu.co>
2019-09-12 10:23:04 -0500

    imprimir y llenar

--- gato advance.cpp
+++ gato advance.cpp
@@ -2,11 +2,33 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
+
+void imprimir (int matriz[8][8]){
+	for (int i = 0; i<8 ;i++){
+		for (int j=0;j<8;j++){
+			cout<<matriz[i][j]<<" ";
+		}
+		cout << ""<<endl;	
+	}
+}
+
+void llenar_matriz (int matriz[8][8]){
+	for (int i = 0; i<8 ;i++){
+		for (int j=0;j<8;j++){
+			matriz[i][j]=0;
+		}			
+	}
+}	
+
+
 int main(int argc, char** argv) {
 	int tablero[8][8];
-	cout << "matriz credada"<<endl;
-	
-	
-	
+	cout << "Matriz credada"<<endl;	
+	cout << "" << endl;	
+	llenar_matriz(tablero);
+	imprimir(tablero);
 	return 0;
 }
+
+
+
