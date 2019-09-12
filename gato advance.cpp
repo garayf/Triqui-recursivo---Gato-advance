b5d7bd03744b6195e8e5f1aa05c553fc0cb85e4f
Garayf <edgarayf@unal.edu.co>
2019-09-12 09:49:13 -0500

    primer comit archivo inicial

---
+++ gato advance.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	int tablero[8][8];
+	cout << "matriz credada"<<endl;
+	
+	
+	
+	return 0;
+}
